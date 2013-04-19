528937f2f2b4405e1c80e2c9e3d5a67fa66fb583
Redezem <AtreusRedezem@redezria.net>
2013-04-19 12:37:38 +0800

    Added Clang and others

--- Matrix/BFSNeo.cpp
+++ Matrix/BFSNeo.cpp
@@ -36,37 +36,61 @@ void SearchNode::InsertChildren(int inNumOfChildren, SearchNode** ChildrenList)
 
 //BFSNeo
 
-void BFSNeo::Explore(SearchNode* inSearch)
+void BFSNeo::Explore(SearchNode* inSearch, int goal)
 {
 	int numberOfNodes, i, j, childCounter;
 	int* childrenArray;
 	SearchNode** OverChildren;
 	SearchNode* Child;
 
-	childCounter=0;
-	numberOfNodes=morphey->GetNumOfNodes();
-	childrenArray=morphey->GetConnectionFieldFrom(inSearch->CheckNumber());
-
-	for(i=0;i<numberOfNodes;i++)
+	if(inSearch->nodeNumber==goal)
 	{
-		if(childrenArray[i]==1)
-		{
-			childCounter++;
-		}
-	}
-	OverChildren=new SearchNode*[childCounter];
-	j=0;
+		searchCompleteFlag=1;
+	}else{
 	
-	for(i=0;i<numberOfNodes;i++)
-	{
-		if(childrenArray[i]==1)
+		childCounter=0;
+		numberOfNodes=morphey->GetNumOfNodes();
+		childrenArray=morphey->GetConnectionFieldFrom(inSearch->CheckNumber());
+
+		for(i=0;i<numberOfNodes;i++)
 		{
-			Child=new SearchNode(i, inSearch);
-			NewExtremities[extremityTicker]=Child;
-			extremityTicker++;
-			OverChildren[j]=Child;
-			j++;
+			if(childrenArray[i]==1)
+			{
+				childCounter++;
+			}
 		}
+		OverChildren=new SearchNode*[childCounter];
+		j=0;
+		
+		for(i=0;i<numberOfNodes;i++)
+		{
+			if(childrenArray[i]==1)
+			{
+				Child=new SearchNode(i, inSearch);
+				NewExtremities[extremityTicker]=Child;
+				extremityTicker++;
+				OverChildren[j]=Child;
+				j++;
+			}
+		}
+		inSearch->InsertChildren(childCounter, OverChildren);
 	}
-	inSearch->InsertChildren(childCounter, OverChildren);
+}
+
+void BFSNeo::SearchFor(int startNode, int endNode, Morpheus* morphey)
+{
+	SearchNode* firstNode;
+	int i;
+	searchCompleteFlag=0;
+	OldExtremities=new SearchNode*[200];
+	NewExtremities=new SearchNode*[200];
+	for(i=0;i<200;i++)
+	{
+		OldExtremities[i]=NULL;
+		NewExtremities[i]=NULL;
+	}
+	
+	firstNode=new SearchNode(startNode,NULL); //cause it's the first node
+	OldExtremities[0]=firstNode;
+	
 }
